12554466421e76503d0b18ce85fe4e2942014710
muhammadali9900 <muhammadalirapidtack@gmail.com>
2023-12-22 12:52:18 +0500

    first project

---
+++ index.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#for").submit('click', function (event) {
+        var userName = document.getElementById('username').value;
+        var userEmail = document.getElementById('useremail').value;
+        var userPhone = document.getElementById('userphone').value;
+        var userGender = document.getElementById('gender').value;
+        var userPassword = document.getElementById('userpassword').value;
+
+        var formData = {
+            user_name: userName,
+            emailid: userEmail,
+            mobno: userPhone,
+            gender: userGender,
+            password: userPassword
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://192.168.100.67:8000/api/user-registration",
+            data: formData,
+            dataType: "json",
+            encode: true,
+        })
+
+            .done(function (data) {
+                console.log(data);
+            })
+            .fail(function (error) {
+                console.error("Error:", error);
+            });
+        console.log(formData);
+        event.preventDefault();
+    });
+});
+
